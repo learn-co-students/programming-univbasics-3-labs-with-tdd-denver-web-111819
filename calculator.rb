64afe9f1368ec98d998b847cc43ff6c07f7df9a5
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-10-31 23:49:02 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+# Add your first variable to pass test 1;
+first_number = 10; 
+# Add your second variable to pass test 2;
+second_number = 5;
+# Add your third variable to pass test 3;
+sum = first_number + second_number;
+#create your next three variables to pass test 4-6;
+difference = first_number - second_number;
+product = first_number * second_number;
+quotient = first_number / second_number;
+
